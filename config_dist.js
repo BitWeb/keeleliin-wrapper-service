f618751b4879837637f57b95c24bf1f9a873fa4e
priit <priit@bitweb.ee>
2015-05-28 12:25:00 +0300

    INIT

---
+++ config_dist.js
@@ -0,0 +1,14 @@
+/**
+ * Created by priit on 26.05.15.
+ */
+
+var config = {};
+
+config.port = 8000;
+
+config.service = {
+    title: 'Teenuse wrapper',
+    description: 'Wrapperi kirjeldus'
+};
+
+module.exports = config;
