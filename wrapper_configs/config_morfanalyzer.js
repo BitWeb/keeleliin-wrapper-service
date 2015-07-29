b04efc9b9d4a2a0666273ddc3f04c08381834af7
priit <priit@bitweb.ee>
2015-07-29 16:58:24 +0300

    Erinevate teenuste confi refactor + lausestaja

--- wrapper_configs/config_morfanalyzer.js
+++ wrapper_configs/config_morfanalyzer.js
@@ -2,5 +2,5 @@ var config = require('./config_global');
 config.port = 3003;
 config.service.title = 'Morfoloogiline analüüs';
 config.service.staticParams.uniqueId = 'morfanalyzer';
-config.service.staticParams.wrapper = config.availableWappers.OSALAUSESTAJA.class;
+config.service.staticParams.wrapper = config.availableWappers.MORFANALYZER.class;
 module.exports = config;
