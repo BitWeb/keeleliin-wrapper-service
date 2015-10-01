d4dd3d4021be4d27293ba83652a26271c7ca7d6e
priit <priit@bitweb.ee>
2015-10-01 18:51:55 +0300

    Teenuse install

--- www/server.js
+++ www/server.js
@@ -82,7 +82,7 @@ function startCluster( instanceCount, cb ){
             installService.install(function () {
                 log4jsLogger.trace('Instal callback');
             })
-        }, Math.random() * 20000);
+        }, ((Math.random() * 20000) + 5000 ));
 
     } else {
         startInstance(cb)
